b1708f967e1c13eeacb0e40a8c92f461f8ec8d67
Bonaventure Umolu <bonaventure.umolu@gmail.com>
2023-02-28 21:43:32 +0100

    chore:

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -11,7 +11,7 @@ definePageMeta({
       <div class="container p-1">
         <div class="row g-0 align-items-center justify-content-center">
           <div class="col-lg-5">
-            <WidgetsQuickActions style="margin-top: -92.2px" />
+            <WidgetsQuickActions style="margin-top: -92.2px" class="mb-4"/>
             <WidgetsActivities
               class="rounded rounded-4"
               :data="useActivity().value.slice(0, 10)"
