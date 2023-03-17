b6b4342da9bff83e557e398b63c679872c4e1889
Bonaventure Umolu <bonaventure.umolu@gmail.com>
2023-03-17 12:07:34 +0100

    chore:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -16,6 +16,10 @@ export default defineNuxtConfig({
             ],
             link: [
                 {
+                    rel: 'digitalAssetLinks',
+                    href: '/assetlinks.json'
+                },
+                {
                     rel: 'stylesheet',
                     href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css',
                     integrity: 'sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65',
