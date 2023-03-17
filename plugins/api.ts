a62b4143e6e10b9d0711efef016841d00fe14352
Bonaventure Umolu <bonaventure.umolu@gmail.com>
2023-03-17 12:03:52 +0100

    chore:

--- plugins/api.ts
+++ plugins/api.ts
@@ -26,11 +26,15 @@ export default defineNuxtPlugin((nuxtApp) => {
         onResponseError({ request, response, options }) {
             // Handle the response errors
             isLoading.value = false;
-            
+
             useToast('error', response._data.message)
 
             if (([403].includes(response.status) && !user.value.email_verified_at)) {
                 navigateTo('verify-email');
+            }
+
+            if ([403].includes(response.status)) {
+                useAuth().logout()
             }
         }
     }
